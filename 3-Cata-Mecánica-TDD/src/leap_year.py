07b080b15bbf1a6014ee1a675305c229a548e4dc
Jorge Ballester <jorge.ballcor@gmail.com>
2024-11-22 10:34:30 +0100

    refactor: all years divisible by 4 are leap years

--- 3-Cata-Mecánica-TDD/src/leap_year.py
+++ 3-Cata-Mecánica-TDD/src/leap_year.py
@@ -8,9 +8,16 @@ def is_leap_year(year: int) -> bool:
     Returns:
         bool: True or False
     """
-    if year == 2008 or year == 2012 or 2016:
+    if (year % 4) == 0:
         return True
     else:
         return False
     
+ #Once we got 3 particular cases -> General rule (line 11)
+ #not before than 3 particular cases  
  
+ #Then see if test pass
+ #If test pass, you can commit changes
+ 
+ #Refactor conducted from GREEN to GREEN
+ #We didnt broke anything
